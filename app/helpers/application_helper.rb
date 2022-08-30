2aed2ab3acbe8cbd317932c9737726a998921c6e
Luiz Guim <luizrmiug@gmail.com>
2022-08-30 17:29:14 +0200

    meta tags done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'Goalkeeper',
+      title: 'Goalkeeper',
+      reverse: true,
+      separator: '|',
+      description: 'The tool to help you better manage your goals!',
+      keywords: 'productive, organize, lifestyle',
+      canonical: request.original_url,
+      noindex: !Rails.env.production?,
+      icon: [
+        { href: image_url('goals.jpeg'), rel: 'touch-icon', sizes: '180x180', type: 'image/jpeg' },
+      ],
+      og: {
+        site_name: 'Goalkeeper',
+        title: 'Goalkeeper',
+        description: 'The tool to help you better manage your goals!',
+        type: 'website',
+        url: request.original_url,
+        image: image_url('goals.jpeg')
+      }
+    }
+  end
 end
