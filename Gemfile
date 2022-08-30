e8dfa8f04c4707f242dcc9dbd8b8419002a1f898
Xaver Briglmeier <x.briglmeier@hotmail.de>
2022-08-30 12:56:56 +0200

    seed file created

--- Gemfile
+++ Gemfile
@@ -18,6 +18,9 @@ gem "puma", "~> 5.0"
 # Bundle and transpile JavaScript [https://github.com/rails/jsbundling-rails]
 gem "jsbundling-rails"
 
+# faker
+gem "faker"
+
 # Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
 gem "turbo-rails"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "open-uri"
+
+User.destroy_all
+Goal.destroy_all
+Task.destroy_all
+
+# generate random users
+puts "Creating users..."
+5.times do
+  user = User.create(email: Faker::Internet.email, password: "123456", user_name: Faker::Name.first_name)
+  puts "Created #{user.user_name}"
+end
+
+puts "Finished!"
+# , last_name: Faker::Name.last_name
+
+# generate random meals
+puts "Creating goals..."
+5.times do
+  goal = Goal.create(name: Faker::Sports::Football.team, description: Faker::Address.full_address, category: "Diet", status: "In Progress", user: User.all.sample)
+  puts "Created #{goal.name}"
+end
+
+puts "Creating tasks..."
+5.times do
+  task = Task.create(name: Faker::Sports::Football.player, description: Faker::Address.full_address, status: "In Progress", goal: Goal.all.sample)
+  puts "Created #{task.name}"
+end
+
+puts "Finished!"
