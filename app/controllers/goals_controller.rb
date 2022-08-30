96e891b016233f1aa2ac86db5ec051b7d955b70a
Natalia Szczepanowska <szczepanowska.n@gmail.com>
2022-08-30 14:32:44 +0200

    goal crud finished

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,10 +1,15 @@
 class GoalsController < ApplicationController
+  before_action :set_goal, only: [:show, :edit, :update, :destroy]
 
   def index
     @goals = current_user.goals
   end
 
   def new
+    @goal = Goal.new
+  end
+
+  def show
     @goal = Goal.new
   end
 
@@ -19,11 +24,29 @@ class GoalsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @goal.update(goal_params)
+      redirect_to goal_path(@goal)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @goal.destroy
+    redirect_to user_path(@goal.user)
+  end
 
   private
 
-  def goal_params
-    params.require(:goal).permit(:name, :description, :category, :start_date, :end_date)
+  def set_goal
+    @goal = Goal.find(params[:id])
   end
 
+  def goal_params
+    params.require(:goal).permit(:name, :description, :category, :start_date, :end_date, :user)
+  end
 end
