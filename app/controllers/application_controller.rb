79033684d973586124c1d6edd1971e7d9d5d1ebf
Carlos Baez Fumero <carlos.baez.fumero@gmail.com>
2022-08-31 17:29:34 +0200

    short end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
+  end
+
   include Pundit::Authorization
 
   # Pundit: allow-list approach
