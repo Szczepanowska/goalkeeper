efe17f3c0231b118754a2c48a21ca656cc348848
Xaver Briglmeier <x.briglmeier@hotmail.de>
2022-09-01 09:30:42 +0200

    added defaults to admin

--- db/migrate/20220831122751_add_admin_to_users.rb
+++ db/migrate/20220831122751_add_admin_to_users.rb
@@ -1,5 +1,5 @@
 class AddAdminToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :admin, :boolean
+    add_column :users, :admin, :boolean, null: false, default: false
   end
 end
