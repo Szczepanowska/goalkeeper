e24def8292f9abb63573a76a9badf0c91e3bdbfa
Carlos Baez Fumero <carlos.baez.fumero@gmail.com>
2022-09-05 10:51:40 +0200

    added working bookmark div

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,23 @@
 class BookmarksController < ApplicationController
   def new
-    @goal = Goal.find()
+    @bookmark = Bookmark.new
+    authorize @bookmark
+  end
+
+  def create
+    @bookmark = Bookmark.new(goal_params)
+    authorize @bookmark
+    @bookmark.user = current_user
+    if @bookmark.save
+      redirect_to home_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    authorize @bookmark
+    @bookmark.destroy
+    redirect_to home_path, status: :see_other
   end
 end
