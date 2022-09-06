1dc3695358168fbd95f9441953eb47aea068c872
Luiz Guim <luizrmiug@gmail.com>
2022-09-06 15:32:13 +0200

    first and last name included

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_01_115705) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_131912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -94,6 +94,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_115705) do
     t.datetime "updated_at", null: false
     t.string "user_name"
     t.boolean "admin", default: false, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
