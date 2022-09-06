d8e7a5cc90a6688801b05a9f05e044d651a3de52
Natalia Szczepanowska <szczepanowska.n@gmail.com>
2022-09-06 09:12:09 +0200

    pg opens when not loged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,10 @@ class PagesController < ApplicationController
       @goals = Goal.where("name ILIKE ?", "%#{params[:query]}%")
     elsif params[:name].present?
       @goals = Goal.where(category: params[:name][:category])
-    else
+    elsif params[:current_user].present?
       @goals = Goal.where("user_id != #{current_user.id}")
+    else
+      @goals = Goal.all
     end
     @goals.order(updated_at: :desc)
   end
